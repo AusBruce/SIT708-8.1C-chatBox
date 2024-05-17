2d64d63a591d66f786664ab8cfa076cba73cd2b3
AusBruce <113293507+AusBruce@users.noreply.github.com>
2024-05-17 13:37:31 +1000

    Initial commit

---
+++ app/src/main/java/com/example/sit708_81c_chatbox/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.example.sit708_81c_chatbox;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChatMessage {
+    private final String message;
+    private final boolean isUser; // true if this message is from the User, false if from the Bot
+
+    public ChatMessage(String message, boolean isUser) {
+        this.message = message;
+        this.isUser = isUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isUser() {
+        return isUser;
+    }
+}
